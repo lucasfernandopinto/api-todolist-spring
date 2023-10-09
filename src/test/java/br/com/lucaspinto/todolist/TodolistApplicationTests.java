df6679719671e5dbfa85c7207296e83ee4efb5e2
Lucas Fernando Pinto <40510593+lucasfpint0@users.noreply.github.com>
2023-10-09 15:57:03 -0300

    Criação do projeto to-do-list e pacote users

---
+++ src/test/java/br/com/lucaspinto/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucaspinto.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
